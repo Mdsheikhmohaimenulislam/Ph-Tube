b6655f2e32957b31bd92469ee6ae17bdda350eee
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-03-14 01:19:26 +0600

    added modal section

--- script/error.js
+++
@@ -1,1 +0,0 @@
-//  document.getElementById("show-error").style.display = "block";
--- script/main.js
+++ script/main.js
@@ -16,7 +16,7 @@ const loadButton = async () => {
     try {
         const fetchlink = await fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
         const data = await fetchlink.json()
-       displayButton(data.categories);
+       displayButton(data.categories)
        
 
     } 
@@ -32,6 +32,10 @@ const loadVideo = async () => {
     try {
         const fetchVideo = await fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         const dataVideo = await fetchVideo.json()
+
+//Example .then((dataVideo) => {  displayVideo(dataVideo.videos)   })  
+
+// {} Curly brackets or braces duita paramiter nice 
         {
             // all button section
             removeActive();
@@ -69,27 +73,51 @@ const loadVideoCategory = async (id) => {
     
 };
 
+// showDetails button
+const showDetails = (videoId) => {
+
+    const url =`
+    https://openapi.programming-hero.com/api/phero-tube/video/${videoId}
+
+    `
+
+     const response = fetch(url)
+     const data = response.json()
+     displayDetailsVideo(data.video)
+
+}
 
 
 
-// {
-//     "category_id": "1001",
-//     "video_id": "aaaa",
-//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-//     "title": "Shape of You",
-//     "authors": [
-//     {
-//     "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-//     "profile_name": "Olivia Mitchell",
-//     "verified": ""
-//     }
-//     ],
-//     "others": {
-//     "views": "100K",
-//     "posted_date": "16278"
-//     },
-//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-//     }
+//? Dynamic Section 
+
+
+// displayDetailsVideo
+const displayDetailsVideo = (video) => {
+   document.getElementById("show_display").showModal()
+   const showcontainer = document.getElementById("show_display")
+
+    // const showDescription = document.createElement("p")
+    showcontainer.innerHTML =`
+            <!-- Open the modal using ID.showModal() method -->
+<!-- <button class="btn" onclick="my_modal_1.showModal()">open modal</button> -->
+
+    <dialog id="show_display" class="modal">
+  <div id="description"  class="modal-box">
+    <h3 class="text-lg pb-4 font-bold text-center">Description</h3>
+    <p>${video.title}</p>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+
+    `
+// showDescriptionBox.appendChild(showDescription)
+}
 
 
 
@@ -105,30 +133,33 @@ const displayVideo = async (videos) => {
 
         // show sms 
         if(videos.length == 0){
+
             videoContainer.innerHTML = `
             <div class="col-span-full py-20 flex flex-col justify-center items-center text-center">
-               <img class="ml-20 mb-3 w-30" src="./assets/Icon.png" alt="">
-               <h1 class="font-bold text-2xl">Oops!! Sorry, There is no content here</h1>
-            </div>
+            <img class="ml-20 mb-3 w-30" src="./assets/Icon.png" alt="">
+            <h1 class="font-bold text-2xl">Oops!! Sorry, There is no content here</h1>
+         </div
 
-             `;
-            return;
+         `
         }
-  
+
 
         for(let video of videos){
 
             const videoContainer = document.createElement("div");
+            
             videoContainer.innerHTML =`
-                <div class="card bg-base-100 space-x-5 space-y-5 mb-5">
+                <div class="card bg-base-100 ">
+                <div class="space-x-5 space-y-5 mb-5 mt-8">
                     <figure class = "relative">
-                      <img class ="w-full h-[150px] object-cover"
+                      <img class ="w-full h-[150px] object-cover rounded "
                         src="${video.thumbnail}"
                         alt="thumbnail" />
                         <span class = "absolute bottom-2 right-2 bg-black text-white p-1 rounded">3hrs 56 min ago</span>
                     </figure>
+                    <div class="card-body  p-0">
                     <div class="card-body flex-row gap-5 px-1 py-0">
-                         <div class="avatar">
+                        <div class="avatar">
                            <div class="ring-primary ring-offset-base-100 w-9 h-9 rounded-full ring ring-offset-2">
                              <img src=${video.authors[0].profile_picture} />
                            </div>
@@ -137,8 +168,16 @@ const displayVideo = async (videos) => {
                             <h1 class ="text-base font-bold">${video.title}</h1>
                             <p class ="flex gap-2 text-sm text-[#17171790]">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=64&id=eZo3c88c63il&format=png" /></p>
                             <p class ="text-sm text-[#17171790]">${video.others.views} views</p>
+                           
                         </div>
                     </div>
+                        <div class="p-2 m-0"> 
+                        <button onclick= showDetails('${video.video_id}')  id="show-details" class="btn btn-block">Show Details</button>
+                        </div>
+                    </div>
+                    
+                </div>
+                    
                 </div>
 
             `;
